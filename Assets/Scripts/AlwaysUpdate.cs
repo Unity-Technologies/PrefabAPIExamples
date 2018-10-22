d7ed1ff936bc86828a78f8303d811c801932a96a
Steen Lund <steenl@unity3d.com>
2018-10-22 15:13:41 -0700

    AlwaysUpdate example

---
+++ Assets/Scripts/AlwaysUpdate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[ExecuteAlways]
+public class AlwaysUpdate : MonoBehaviour
+{
+    public float rotationSpeed = 20.0f;
+
+    [System.NonSerialized]
+    public float updateCount = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Rotate only in playmode
+        // We don't want this in edit mode nor in prefab mode
+        // as it would update the prefab asset
+        if (Application.IsPlaying(this))
+        {
+            Transform t = GetComponent<Transform>();
+            t.Rotate(rotationSpeed * Time.deltaTime, 0, 0);
+        }
+
+        ++updateCount;
+        Debug.Log(string.Format("Updates {0}", updateCount));
+    }
+}
