44f6ccb0bff230667ae040838226b20130895c12
Steen Lund <steenl@unity3d.com>
2018-10-22 14:54:24 -0700

    Add custom prefab environment

---
+++ Assets/Editor/Scripts/CustomPrefabEnvironment.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+using UnityEditor.Experimental.SceneManagement;
+ 
+ 
+[InitializeOnLoad]
+class CustomPrefabEnvironment
+{
+    static CustomPrefabEnvironment()
+    {
+        PrefabStage.prefabStageOpened += OnPrefabStageOpened;
+    }
+ 
+    static void OnPrefabStageOpened(PrefabStage prefabStage)
+    {
+        Debug.Log("OnPrefabStageOpened " + prefabStage.prefabAssetPath);
+ 
+        // Get info from the PrefabStage
+        var root = prefabStage.prefabContentsRoot;
+        var scene = prefabStage.scene;
+        var renderer = root.GetComponent<Renderer>();
+ 
+        // If no renderer skip our custom environment
+        if (renderer == null)
+            return;
+ 
+        // Create environment plane
+        var plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
+        SceneManager.MoveGameObjectToScene(plane, scene);
+ 
+        // Adjust environment plane to the prefab root's lower bounds
+        Bounds bounds = renderer.bounds;
+        plane.transform.position = new Vector3(bounds.center.x, bounds.min.y, bounds.center.z);
+    }
+}
