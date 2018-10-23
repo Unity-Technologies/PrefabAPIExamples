7f6b164ca095d7e8806b693374a9bb513110da92
Steen Lund <steenl@unity3d.com>
2018-10-23 03:54:21 -0700

    Example of how to update the prefab assets if the library objects have been modified.

---
+++ Assets/Editor/Scripts/ModifyLibraryObject.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class ModifyLibraryObject
+{
+    [MenuItem("Prefabs/Modify/Library Object")]
+    static public void DontDoThis()
+    {
+        string path = "Assets/Prefabs/A.prefab";
+        var goA = (GameObject)AssetDatabase.LoadMainAssetAtPath(path);
+
+        GameObjectUtility.SetStaticEditorFlags(goA, ~GameObjectUtility.GetStaticEditorFlags(goA));
+        PrefabUtility.SavePrefabAsset(goA);
+    }
+
+    [MenuItem("Prefabs/Modify/Library Object B inside A")]
+    static public void DontDoThisAtAll()
+    {
+        string path = "Assets/Prefabs/A.prefab";
+        var goA = (GameObject)AssetDatabase.LoadMainAssetAtPath(path);
+
+        var goB = goA.GetComponent<Transform>().GetChild(0).gameObject;
+
+        GameObjectUtility.SetStaticEditorFlags(goB, ~GameObjectUtility.GetStaticEditorFlags(goB));
+
+        // B is a nested prefab inside A
+        // So doing this resulsts in PropertyModifications being generate for B inside A
+        PrefabUtility.SavePrefabAsset(goA);
+    }
+}
