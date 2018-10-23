f219208fcdbc127488d956eee7753afb0a9eea3d
Steen Lund <steenl@unity3d.com>
2018-10-23 03:17:07 -0700

    Nesting examples

---
+++ Assets/Editor/Scripts/CreateNesting.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class CreateNesting
+{
+    [MenuItem("Prefabs/Nesting/NestBInA Via Instances")]
+    static public void NestBInAViaInstances()
+    {
+        var goA = (GameObject)AssetDatabase.LoadMainAssetAtPath("Assets/Prefabs/A.prefab");
+        var goB = (GameObject)AssetDatabase.LoadMainAssetAtPath("Assets/Prefabs/B.prefab");
+
+        var instanceA = (GameObject)PrefabUtility.InstantiatePrefab(goA);
+        var instanceB = (GameObject)PrefabUtility.InstantiatePrefab(goB);
+
+        instanceB.GetComponent<Transform>().parent = instanceA.GetComponent<Transform>();
+        PrefabUtility.ApplyPrefabInstance(instanceA, InteractionMode.AutomatedAction);
+    }
+
+    [MenuItem("Prefabs/Nesting/NestBInA Via Content")]
+    static public void NestBInAViaContent()
+    {
+        string path = "Assets/Prefabs/A.prefab";
+
+        var goB = (GameObject)AssetDatabase.LoadMainAssetAtPath("Assets/Prefabs/B.prefab");
+        var instanceB = (GameObject)PrefabUtility.InstantiatePrefab(goB);
+
+        var root = PrefabUtility.LoadPrefabContents(path);
+
+        instanceB.GetComponent<Transform>().parent = root.GetComponent<Transform>();
+        PrefabUtility.SaveAsPrefabAsset(root, path);
+
+        PrefabUtility.UnloadPrefabContents(root);
+    }
+}
